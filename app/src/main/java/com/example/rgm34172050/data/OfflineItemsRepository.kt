d6a2583db19165f02eaf706cc08661ef4ef01005
thigasth <141834703+thigasth@users.noreply.github.com>
2024-11-25 18:00:09 -0300

    OfflineItemsRepository

--- app/src/main/java/com/example/rgm34172050/data/OfflineItemsRepository.kt
+++ app/src/main/java/com/example/rgm34172050/data/OfflineItemsRepository.kt
@@ -1,9 +1,19 @@
 package br.edu.up.rgm34353470.data
 
 
+import com.example.rgm34172050.data.Item
 import com.example.rgm34172050.data.ItemDao
+import com.example.rgm34172050.data.ItemsRepository
+import kotlinx.coroutines.flow.Flow
 
+class OfflineItemsRepository(private val itemDao: ItemDao) : ItemsRepository {
+    override fun getAllItemsStream(): Flow<List<Item>> = itemDao.getAllItems()
 
-class OfflineItemsRepository{
+    override fun getItemStream(id: Int): Flow<Item?> = itemDao.getItem(id)
 
+    override suspend fun insertItem(item: Item) = itemDao.insert(item)
+
+    override suspend fun deleteItem(item: Item) = itemDao.delete(item)
+
+    override suspend fun updateItem(item: Item) = itemDao.update(item)
 }
